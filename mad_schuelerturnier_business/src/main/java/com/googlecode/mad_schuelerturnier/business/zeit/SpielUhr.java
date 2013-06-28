2d2bd5d8e2cbba7e7febde5dc8af71c7fe0dea8a
Daniel Marthaler <marthaler.worb@gmail.com>
2013-06-28 23:58:31 +0200

    einfacher unittest

--- mad_schuelerturnier_business/src/main/java/com/googlecode/mad_schuelerturnier/business/zeit/SpielUhr.java
+++ mad_schuelerturnier_business/src/main/java/com/googlecode/mad_schuelerturnier/business/zeit/SpielUhr.java
@@ -9,14 +9,9 @@ import org.springframework.stereotype.Component;
 import java.text.SimpleDateFormat;
 
 /**
- * mad letzte aenderung: $Date: 2012-01-09 21:02:56 +0100 (Mo, 09 Jan 2012) $
+ * Uhr fuer die Darstellung der Zeit und Verspaetung im GUI
  *
  * @author $Author: marthaler.worb@gmail.com $
- * @version $Revision: 155 $
- * @headurl $HeadURL:
- * https://mad-schuelerturnier.googlecode.com/svn/trunk/mad_schuelereturnier
- * /src/main/java/com/googlecode/mad_schuelerturnier/business/
- * controller/leiter/HTMLSchiriConverter.java $
  */
 @Component
 public class SpielUhr implements ApplicationListener<ZeitPuls> {
@@ -30,13 +25,9 @@ public class SpielUhr implements ApplicationListener<ZeitPuls> {
     private String spielZeit = NOT_INIT;
 
     public void onApplicationEvent(final ZeitPuls event) {
-
-        final ZeitPuls p = event;
-
         final SimpleDateFormat fmt = new SimpleDateFormat("HH:mm:ss");
-        this.richtigeZeit = fmt.format(p.getEchteZeit().toDate());
-        this.spielZeit = fmt.format(p.getSpielZeit().toDate());
-
+        this.richtigeZeit = fmt.format(event.getEchteZeit().toDate());
+        this.spielZeit = fmt.format(event.getSpielZeit().toDate());
     }
 
     public String getRichtigeZeit() {
@@ -45,6 +36,14 @@ public class SpielUhr implements ApplicationListener<ZeitPuls> {
 
     public String getSpielZeit() {
         return this.spielZeit;
+    }
+
+    @Override
+    public String toString() {
+        return "SpielUhr{" +
+                "richtigeZeit='" + richtigeZeit + '\'' +
+                ", spielZeit='" + spielZeit + '\'' +
+                '}';
     }
 
 }
