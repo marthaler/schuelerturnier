b97cd92b18d358ad5613d0199bcc25fe5e6430ce
Daniel Marthaler <marthaler.worb@gmail.com>
2013-09-27 23:34:05 +0200

    print mannschaften auf 25

--- mad-schuelerturnier-resultengine/src/main/java/com/googlecode/madschuelerturnier/business/templateengine/DateTool.java
+++ mad-schuelerturnier-resultengine/src/main/java/com/googlecode/madschuelerturnier/business/templateengine/DateTool.java
@@ -3,19 +3,6 @@
  */
 package com.googlecode.madschuelerturnier.business.templateengine;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.log4j.Logger;
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.runtime.RuntimeConstants;
-import org.apache.velocity.runtime.RuntimeServices;
-import org.apache.velocity.runtime.RuntimeSingleton;
-import org.apache.velocity.runtime.parser.node.SimpleNode;
-
-import java.io.BufferedInputStream;
-import java.io.InputStream;
-import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
