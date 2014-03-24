d0b4b643c3ce32463e94ff2420651581255febb3
Daniel Marthaler <marthaler.worb@gmail.com>
2014-03-25 00:06:58 +0100

    websiteInfoController

--- mad-schuelerturnier-web/src/main/java/com/googlecode/madschuelerturnier/web/controllers/WebsiteinfoController.java
+++ mad-schuelerturnier-web/src/main/java/com/googlecode/madschuelerturnier/web/controllers/WebsiteinfoController.java
@@ -5,42 +5,43 @@
 /**
  * Apache License 2.0
  */
-package com.googlecode.madschuelerturnier.web.mvc;
+package com.googlecode.madschuelerturnier.web.controllers;
 
 import com.googlecode.madschuelerturnier.business.websiteinfo.WebsiteInfoService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Controller zum Downloaden der Mannschaftscouverts als C5
+ * Controller fuer die Webseiteninformationen
  *
  * @author marthaler.worb@gmail.com
  * @since 1.2.8
  */
-@Controller
-public class SiteController {
+@Component
+@RequestMapping("/website")
+public class WebsiteinfoController {
 
-    private static final Logger LOG = Logger.getLogger(SiteController.class);
+    private static final Logger LOG = Logger.getLogger(WebsiteinfoController.class);
 
     @Autowired
     private WebsiteInfoService service;
 
-    @RequestMapping(value = "/websiteinfo/${jahr}", method = RequestMethod.GET)
-    public String getWebsiteinfo(@PathVariable String jahr, Model model) {
+    @RequestMapping(value = "/info/{jahr}", method = RequestMethod.GET)
+    public String getWebsiteinfo(@PathVariable("jahr") String jahr, Model model) {
 
         if(jahr.length() < 4){
             jahr = null;
         }
 
         model.addAttribute("teams", service.getGeschlechtgruppen(jahr));
-        return "websiteinf/anzeige.xhtml";
+        return "website/info.xhtml";
 
     }
-
 
 }
