23550267d004021eb128e5825bbb5a761e1d1b3d
u203244 <daniel.marthaler1@sbb.ch>
2015-06-08 07:47:39 +0200

    configuration des webcam remote service

--- emad-schuetu-business/src/main/java/ch/emad/business/schuetu/SpringContextBusinessSchuetu.java
+++ emad-schuetu-business/src/main/java/ch/emad/business/schuetu/SpringContextBusinessSchuetu.java
@@ -6,9 +6,10 @@ import ch.emad.business.schuetu.picture.WebcamBusinessImpl;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
-//@Configuration
+@Configuration
 public class SpringContextBusinessSchuetu {
 
     private static final Logger LOG = Logger.getLogger(SpringContextBusinessSchuetu.class);
--- emad-schuetu-web-picimport/src/main/java/ch/emad/business/SpringContextBusinessPicimport.java
+++ emad-schuetu-web-picimport/src/main/java/ch/emad/business/SpringContextBusinessPicimport.java
@@ -5,7 +5,9 @@
 
 package ch.emad.business;
 
+import ch.emad.business.common.properties.PropProvider;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.rmi.RmiProxyFactoryBean;
@@ -15,11 +17,35 @@ public class SpringContextBusinessPicimport {
 
     private static final Logger LOG = Logger.getLogger(SpringContextBusinessPicimport.class);
 
+    @Autowired
+    private PropProvider props;
+
     @Bean()
     public RmiProxyFactoryBean initBean(){
+
+        String port = "335";
+        String stage = props.readString("st");
+
+        if (stage.equals("d")) {
+            port = port + "0";
+        }
+
+        if (stage.equals("t")) {
+            port = port + "1";
+        }
+
+        if (stage.equals("i")) {
+            port = port + "2";
+        }
+
+        if (stage.equals("p")) {
+            port = port + "3";
+        }
+
+        LOG.info("set the port for webcamsource: " + Integer.parseInt(port));
+
         org.springframework.remoting.rmi.RmiProxyFactoryBean exp = new org.springframework.remoting.rmi.RmiProxyFactoryBean();
-        exp.setServiceUrl("rmi://87.230.15.247:3352/WebcamBusiness");
-        //exp.setServiceUrl("rmi://localhost:3350/WebcamBusiness");
+        exp.setServiceUrl("rmi://87.230.15.247:"+port+"/WebcamBusiness");
         exp.setServiceInterface(WebcamBusiness.class);
         return exp;
     }
